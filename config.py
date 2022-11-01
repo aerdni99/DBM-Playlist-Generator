5a1d83b052754b4e79c0b26b9f005eac915268f6
Ryan Dang <rydang@umich.edu>
2022-11-01 18:30:22 -0400

    made config contain strings

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-host = ec2-54-163-34-107.compute-1.amazonaws.com
+host = 'ec2-54-163-34-107.compute-1.amazonaws.com'
 port = 5432
-database = dokaffa4bdtr5
-user = aiertbbbpvwith
-password = 97b7d375e7f3a5e77c209f7c3849ce32ca245331c096709e77aab93119ad3b21
+database = 'dokaffa4bdtr5'
+user = 'aiertbbbpvwith'
+password = '97b7d375e7f3a5e77c209f7c3849ce32ca245331c096709e77aab93119ad3b21'
