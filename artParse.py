f8e787fa6e7143ee82799ee9fb584bc0a49b862b
Angelo <97480357+aerdni99@users.noreply.github.com>
2022-11-29 17:18:10 -0500

    Update artParse.py

--- artParse.py
+++ artParse.py
@@ -1,33 +1,50 @@
+"""
+    artParse.py
+
+    Parsing loop for transferring data from teacks_features.csv to our Created_By table
+"""
 
 import psycopg2
-import re
 import sys
 import csv
 
 try:
     
-    con = psycopg2.connect("host=ec2-54-163-34-107.compute-1.amazonaws.com dbname=dokaffa4bdtr5 user=aiertbbbpvwith password=97b7d375e7f3a5e77c209f7c3849ce32ca245331c096709e77aab93119ad3b21")
+    # EDIT CONNECTION INFO FOR SETTING UP ON A DIFFERENT SERVER
+    con = psycopg2.connect(
+        database="postgres",
+        user="postgres",
+        password="password")
 
     cur = con.cursor()
-    count = 0
+    numRows = 1000
+    count = -1
 
-    with open("tracks_features.csv") as csv_file:
+    with open("tracks_features.csv", encoding="utf8") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             count += 1
-            if (count > 1000):
+            if count > numRows:
                 break
+            values = ""
+            if count == 0:
+                continue
             attribCount = 0
             for x in row:
                 attribCount += 1
                 if (attribCount == 1):
                     # Handle song ID
-                    songID = '\'' + x + '\''
+                    songID = "\'" + x + "\'"
                 if (attribCount == 5):
-                    # Handle artist names
-                    index = x.find('\', \'') + 3
-# To be continued...
+                    index = 0
+                    for y in range(x.count('\', \'') + 1):
+                        # Handle artist names
+                        artistName = "\'" + x[(x.find('\'') + 1):(-1 * (len(x) - x.find('\'', x.find('\'') + 1)))] + "\'"
+                        x = x[(x.find('\'', x.find('\'') + 1)) + 3:]
+                        query = 'INSERT INTO public."Created_By" VALUES (' + artistName + ', ' + songID + ');'
+                        cur.execute(query)
 
+    # con.commit()
 
 except psycopg2.DatabaseError as e:
 
@@ -39,4 +56,3 @@ finally:
     if con:
         con.close()
             
-
