f5a2261a5fd15a3b6bc1419d7dd17593c54aaa24
Ryan Dang <rydang@umich.edu>
2022-11-30 05:24:27 -0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# This connection info is outdated
-
-host = 'ec2-54-163-34-107.compute-1.amazonaws.com'
-port = 5432
-database = 'dokaffa4bdtr5'
-user = 'aiertbbbpvwith'
-password = '97b7d375e7f3a5e77c209f7c3849ce32ca245331c096709e77aab93119ad3b21'
